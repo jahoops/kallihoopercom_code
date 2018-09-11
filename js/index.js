8ae4800c78fc198fda9d1bf3c96f4ad55e75519b
jh <jwhooper@gmail.com>
2018-09-11 16:21:39 -0400

    up

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
   function pagesize() {
     var w = window.innerWidth;
     var top = w / 5 + 'px';
-    var fontsize = 14 * w/1400 + 'px';
+    var fontsize = 14 * w / 1400 + 'px';
     var t = $('#Typewriter');
     $(t).css({
       'top': top,
@@ -10,10 +10,34 @@
     });
   }
 
-  $(window).on('resize', function(){
+  $(window).on('resize', function () {
     pagesize();
   });
 
   pagesize();
+
+  $('#pagepiling').pagepiling({
+    menu: '#menu',
+    anchors: ['Typewriter', 'leadership', 'cad', 'javascript', 'drum', 'desert', 'blurred','excel', 'gallery', 'contact'],
+    sectionsColor: ['#bfda00', '#2ebe21', '#2C3E50', '#51bec4','#bfda00', '#2ebe21', '#2C3E50', '#51bec4','#bfda00', '#2ebe21'],
+    direction: 'horizontal',
+    navigation: {
+      'position': 'right',
+      'tooltips': ['Home', 'Leadership', 'CAD', 'JavaScript','Sound', 'Clock','Blur', 'Excel', 'Gallery', 'Contact']
+    },
+    afterRender: function () {
+      //playing the video
+      console.log('callback after render');
+    }
+  });
+
+  $('.clockprojecticon').on('mouseenter', function () {
+    if ($('#clock').css('top') !== '3vh') {
+      $('#clock').css('top', '3vh');
+    }
+  });
+  $('.clockprojecticon').on('mouseleave', function () {
+    $('#clock').css('top', '250vh');
+  });
 
 })();
