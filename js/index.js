e8692239f6079f44b9cafc2845c325777952d3f5
jahoops <jwhooper@gmail.com>
2018-09-12 22:50:58 -0400

    up

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
-(function () {
+$(function () {
+
+  $('#pagepiling').pagepiling({
+    menu: '#menu',
+    anchors: ['home', 'leadership', 'cad', 'drum', 'desert', 'blurred','excel', 'gallery', 'contact'],
+    sectionsColor: ['rgb(235, 230, 221)', 'rgb(235, 230, 221)', 'rgb(235, 230, 221)', 'rgb(235, 230, 221)',
+    'rgb(235, 230, 221)', 'rgb(235, 230, 221)', 'rgb(235, 230, 221)', 'rgb(235, 230, 221)','rgb(235, 230, 221)'],
+    direction: 'horizontal',
+    navigation: {
+      'position': 'right',
+      'tooltips': ['Home', 'Leadership', 'CAD', 'Sound', 'Clock','Blur', 'Excel', 'Gallery', 'Contact']
+    },
+    afterRender: function () {   
+    }
+  });
+
   function pagesize() {
-    var w = window.innerWidth;
-    var top = w / 5 + 'px';
-    var fontsize = 14 * w / 1400 + 'px';
+    var iw = $('#home').innerWidth();
+    var w = iw/1400; 
+    console.log(iw,w);
+    var top =  400*w + 'px';
+    var fontsize = 14 * w + 'px';
     var t = $('#typewriter');
     $(t).css({
       'top': top,
@@ -16,21 +33,6 @@
 
   pagesize();
 
-  $('#pagepiling').pagepiling({
-    menu: '#menu',
-    anchors: ['home', 'leadership', 'cad', 'javascript', 'drum', 'desert', 'blurred','excel', 'gallery', 'contact'],
-    sectionsColor: ['', 'rgb(235, 230, 221)', '#2C3E50', '#51bec4','#bfda00', '#2ebe21', '#2C3E50', '#51bec4','#bfda00', '#2ebe21'],
-    direction: 'horizontal',
-    navigation: {
-      'position': 'right',
-      'tooltips': ['Home', 'Leadership', 'CAD', 'JavaScript','Sound', 'Clock','Blur', 'Excel', 'Gallery', 'Contact']
-    },
-    afterRender: function () {
-      //playing the video
-      console.log('callback after render');
-    }
-  });
-
   $('.clockprojecticon').on('mouseenter', function () {
     if ($('#clock').css('top') !== '3vh') {
       $('#clock').css('top', '3vh');
@@ -40,4 +42,36 @@
     $('#clock').css('top', '250vh');
   });
 
-})();
+  //gallery
+  var elem = document.querySelector('#photos');
+  var msnry = new Masonry( elem, {
+  });
+
+  //clock
+  var secondHand = document.querySelector('.second-hand');
+  var minuteHand = document.querySelector('.min-hand');
+  var hoursHand = document.querySelector('.hour-hand');
+  var now = new Date();
+  var lastsecond = now.getSeconds();
+
+  function clock() {
+    var now = new Date();
+    var seconds = now.getSeconds();
+    if(seconds !== lastsecond){
+      lastsecond = seconds;  
+      var secondsDegrees = ((seconds / 60) * 360) + 90;
+      secondHand.style.transform = 'rotate(' + secondsDegrees +'deg)';
+      var mins = now.getMinutes();
+      var minutesDegrees = ((mins / 60) * 360) + 90;
+      minuteHand.style.transform = 'rotate(' + minutesDegrees +'deg)';
+      var hours = now.getHours();
+      var hoursDegrees = ((hours / 12) * 360) + 90;
+      hoursHand.style.transform = 'rotate(' + hoursDegrees +'deg)';
+    }
+  
+    requestAnimationFrame(clock);
+  }
+
+  requestAnimationFrame(clock);
+  
+});
